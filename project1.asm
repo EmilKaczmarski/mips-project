2381d1c22067dfc04df9e42410043a8c38dd2d92
EmilKaczmarski <51459339+EmilKaczmarski@users.noreply.github.com>
2021-05-03 19:04:45 +0200

    almost final verison

--- project1.asm
+++ project1.asm
@@ -1,17 +1,18 @@
 .data
 
 .eqv BITMAP_HEADER_SIZE 54
+.eqv ROW_SIZE 960
 bitmap_header: .space BITMAP_HEADER_SIZE
 
 bitmap_digit0: # '0'
-.byte 0, 1, 1, 1, 1, 1, 1, 0
-.byte 0, 1, 0, 0, 0, 0, 1, 0
-.byte 0, 1, 0, 0, 0, 0, 1, 0
-.byte 0, 1, 0, 0, 0, 0, 1, 0
-.byte 0, 1, 0, 0, 0, 0, 1, 0
-.byte 0, 1, 0, 0, 0, 0, 1, 0
-.byte 0, 1, 0, 0, 0, 0, 1, 0
-.byte 0, 1, 1, 1, 1, 1, 1, 0
+.byte 1, 1, 1, 1, 1, 1, 1, 1
+.byte 1, 0, 0, 0, 0, 0, 0, 1
+.byte 1, 0, 0, 0, 0, 0, 0, 1
+.byte 1, 0, 0, 0, 0, 0, 0, 1
+.byte 1, 0, 0, 0, 0, 0, 0, 1
+.byte 1, 0, 0, 0, 0, 0, 0, 1
+.byte 1, 0, 0, 0, 0, 0, 0, 1
+.byte 1, 1, 1, 1, 1, 1, 1, 1
 
 bitmap_digit1: # '1'
 .byte 0, 0, 0, 1, 0, 0, 0, 0
@@ -84,24 +85,24 @@ bitmap_digit7: # '7'
 .byte 0, 0, 0, 0, 1, 0, 0, 0
 
 bitmap_digit8: # '8'
-.byte 0, 1, 1, 1, 1, 1, 1, 0
-.byte 0, 1, 0, 0, 0, 0, 1, 0
-.byte 0, 1, 0, 0, 0, 0, 1, 0
-.byte 0, 1, 0, 0, 0, 0, 1, 0
-.byte 0, 1, 1, 1, 1, 1, 1, 0
-.byte 0, 1, 0, 0, 0, 0, 1, 0
-.byte 0, 1, 0, 0, 0, 0, 1, 0
-.byte 0, 1, 1, 1, 1, 1, 1, 0
+.byte 1, 1, 1, 1, 1, 1, 1, 0
+.byte 1, 0, 0, 0, 0, 0, 1, 0
+.byte 1, 0, 0, 0, 0, 0, 1, 0
+.byte 1, 0, 0, 0, 0, 0, 1, 0
+.byte 1, 1, 1, 1, 1, 1, 1, 0
+.byte 1, 0, 0, 0, 0, 0, 1, 0
+.byte 1, 0, 0, 0, 0, 0, 1, 0
+.byte 1, 1, 1, 1, 1, 1, 1, 0
 
 bitmap_digit9: # '9'
 .byte 0, 1, 1, 1, 1, 1, 1, 0
-.byte 0, 1, 0, 0, 0, 0, 1, 0
-.byte 0, 1, 0, 0, 0, 0, 1, 0
-.byte 0, 1, 0, 0, 0, 0, 1, 0
-.byte 0, 1, 1, 1, 1, 1, 1, 0
-.byte 0, 1, 0, 0, 0, 0, 0, 0
-.byte 0, 1, 0, 0, 0, 0, 0, 0
-.byte 0, 1, 1, 1, 1, 1, 1, 0
+.byte 1, 0, 0, 0, 0, 0, 1, 0
+.byte 1, 0, 0, 0, 0, 0, 1, 0
+.byte 1, 0, 0, 0, 0, 0, 1, 0
+.byte 1, 1, 1, 1, 1, 1, 1, 0
+.byte 1, 0, 0, 0, 0, 0, 0, 0
+.byte 1, 0, 0, 0, 0, 0, 0, 0
+.byte 1, 1, 1, 1, 1, 1, 1, 0
 
 bitmap_dot: # '.'
 .byte 0, 0, 0, 0, 0, 0, 0, 0
@@ -143,7 +144,6 @@ input_buffer: .space 64
 .text
 
 main:
-
 	li $a0, 1048576 # one mebibyte
 	li $v0, 9
 	syscall                               # allocate memory
@@ -174,14 +174,11 @@ main:
 	lhu $t1, 0($at)
 
 read_pixel_data:
-	
 	la $t0, data_size
 	li $t1, BITMAP_HEADER_SIZE
 	subu $t0, $t0, $t1
 
-	# reads the remainder of the file
-
-	la $at, file_handle_input
+	la $at, file_handle_input # reads the remainder of the file
 	lw $a0, 0($at)
 
 	la $a1, file_buffer
@@ -198,7 +195,6 @@ read_pixel_data:
 	sw $v0, 0($at)
 
 main_pixel_array_offset:
-
 	la $at, file_buffer
 	lw $t0, 0($at)
 
@@ -208,7 +204,6 @@ main_pixel_array_offset:
 	b main_user_input
 
 prompt_error:
-
 	la $a0, error_string
 	li $v0, 4
 	syscall                               # print string
@@ -216,7 +211,6 @@ prompt_error:
 	j main_program_end
 
 main_user_input:
-
 	la $a0, prompt_number
 	li $v0, 4
 	syscall                               # print string
@@ -250,103 +244,76 @@ main_user_input:
 	addiu $s0, $s0, -1
 
 main_draw_text_loop:
-
 	addiu $s0, $s0, 1
-
 	lbu $t0, 0($s0)
 	beq $t0, $zero, main_write_file
 
 	li $t1, '.'
 	beq $t0, $t1, main_draw_text_dot
-
 	li $t1, '9'
 	bgt $t0, $t1, main_draw_text_loop
-
 	li $t1, '0'
 	blt $t0, $t1 main_draw_text_loop
 
 main_draw_text_digit:
-
 	subu $t0, $t0, $t1 # $t0 = index in array_digits
-
-	sll $t0, $t0, 2
+	sll $t0, $t0, 2    
 
 	la $a0, array_digits # $a0 = pointer to bitmap tile
 	addu $a0, $a0, $t0
 	lw $a0, 0($a0)
-
 	la $at, coordinate_x
 	lw $a1, 0($at)
-
 	la $at, coordinate_y
 	lw $a2, 0($at)
 	
 	jal draw_tile
 
-	b main_draw_text_next_tile
-
 main_draw_text_dot:
-
 	la $a0, bitmap_dot
-
 	la $at, coordinate_x
 	lw $a1, 0($at)
-
 	la $at, coordinate_y
 	lw $a2, 0($at)
-	
 	jal draw_tile
 
 main_draw_text_next_tile:
 	la $t0, coordinate_x
 	lw $t1, 0($t0)
-	addiu $t1, $t1, 8
+	addiu $t1, $t1, 16
 	sw $t1, 0($t0)
 
 	b main_draw_text_loop
 
 main_write_file:
-
 	la $a0, file_name_output
 	li $a1, 1
 	li $a2, 0
 	li $v0, 13
 	syscall                               # open file
-
 	blt $v0, $zero, prompt_error
 
 	la $at, file_handle_output
 	sw $v0, 0($at)
-
 	move $a0, $v0
-
 	la $a1, bitmap_header
-
 	li $a2, BITMAP_HEADER_SIZE
-
 	li $v0, 15
 	syscall                               # write file
-
 	blt $v0, $zero, prompt_error
 
 	la $a0, file_handle_output
 	lw $a0, 0($a0)
-
 	la $a1, file_buffer
 	lw $a1, 0($a1)
-
 	la $a2, data_size
 	lw $a2, 0($a2)
-
 	li $v0, 15
 	syscall                               # write file
 
 	b main_program_end
 
-.eqv ROW_SIZE 960
-# $a0: pointer to tile to be printed, $a1: coordinate x, $a2: coordinate y, $a3: color
-draw_tile:
-	# $t4 = pointer to end of pixels array
+draw_tile: # $a0: pointer to tile to be printed, $a1: coordinate x, $a2: coordinate y, $a3: color
 	la $at, pixel_array_pointer
 	lw $t0, 0($at)
 
@@ -357,59 +324,43 @@ draw_tile:
 	multu $a2, $at
 	mflo $a2
 
-	# $t0 = offset
-	li $t0, ROW_SIZE
+	li $t0, ROW_SIZE # $t0 = offset
 	subu $t0, $t0, $a1
 	addu $t0, $t0, $a2
 
-	# $t1 = limit address, marks end of bitmap
 	li $at, 7680 # tile stride (24 * 320)
-	addu $t1, $t0, $at
+	addu $t1, $t0, $at # $t1 = limit address, marks end of bitmap
 
 draw_tile_loop:
-
 	bge $t0, $t1, draw_tile_end
-
 	move $t2, $zero
 	li $t3, 8 # pixels in a row
 
 draw_tile_row_loop:
 	beq $t2, $t3, draw_tile_row_end
-
 	lbu $at, 0($a0)
 	beq $at, $zero, draw_tile_row_loop_continue
 
 draw_tile_black_row_pixel:
-
 	subu $t5, $t4, $t0
-
 	li $a3, 0x00000000
-	
 	sb $a3, 0($t5)
-	srl $a3, $a3, 8
 	sb $a3, 1($t5)
-	srl $a3, $a3, 8
 	sb $a3, 2($t5)
-
-	srl $a3, $a3, 8
 	sb $a3, 3($t5)
 
 draw_tile_row_loop_continue:
-
 	addiu $t0, $t0, 1
 	addiu $t2, $t2, 1
 	addiu $a0, $a0, 1
 	b draw_tile_row_loop
 
 draw_tile_row_end:
-	
 	li $at, ROW_SIZE # moves pointer to the next row
 	addu $t0, $t0, $at
 
-	# moves pointer to start of tile row, at the left
 	li $at, 8 # tile row size
-	subu $t0, $t0, $at
-
+	subu $t0, $t0, $at # moves pointer to start of tile row, at the left
 	b draw_tile_loop
 
 draw_tile_end:
